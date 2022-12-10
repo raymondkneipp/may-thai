546258d379d2cae43478b0332dd3cd825dc163eb
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-09 22:37:44 -0500

    add local font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
+
+const ballantines = localFont({
+  src: "./ballantines-serial-medium-regular.ttf",
+  weight: "400",
+  variable: "--font-ballantines",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${ballantines.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
