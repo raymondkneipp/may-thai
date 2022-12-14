308b33d67c0ffcc8121fdbf9141337b20852c194
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-14 12:47:26 -0500

    change testimonial layout

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -9,14 +9,14 @@ export function Testimonials() {
           Testimonials
         </h2>
 
-        <div className="grid lg:grid-cols-3 gap-12">
+        <div className="grid gap-12">
           {TESTIMONIALS.map((review) => (
-            <blockquote className="shadow-xl shadow-stone-200 p-6 rounded-xl relative flex flex-col items-center gap-3">
+            <blockquote className="shadow-xl shadow-stone-200 p-6 rounded-2xl bg-stone-50 relative flex flex-col items-center gap-3 odd:ml-auto even:mr-auto md:w-2/3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 preserveAspectRatio="xMidYMid meet"
                 viewBox="0 0 24 24"
-                className="w-12 h-12 text-stone-200 top-0 left-0 -translate-x-1/2 -translate-y-1/2 absolute"
+                className="w-16 h-16 text-stone-200 top-0 left-0 -translate-x-1/2 -translate-y-1/2 absolute"
               >
                 <g fill="none">
                   <path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" />
@@ -27,14 +27,14 @@ export function Testimonials() {
                 </g>
               </svg>
               {review.reviewBody}
-              <footer className="text-center text-stone-600 flex-1 flex items-end justify-center">
+              <footer className="text-center text-stone-500 flex-1 flex items-end justify-center">
                 - <cite>{review.author}</cite>
               </footer>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 preserveAspectRatio="xMidYMid meet"
                 viewBox="0 0 24 24"
-                className="w-12 h-12 text-stone-200 bottom-0 right-0 translate-x-1/2 translate-y-1/2 absolute"
+                className="w-16 h-16 text-stone-200 bottom-0 right-0 translate-x-1/2 translate-y-1/2 absolute"
               >
                 <g fill="none">
                   <path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" />
