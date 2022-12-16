cb54947bcc48f70cc8475b70f02065fbdecab054
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-16 16:58:05 -0500

    env typescript

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,44 +1,43 @@
-import { ADDRESS } from "@constants";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
+import { useMemo } from 'react';
 
 export function Map() {
-  const center = useMemo(
-    () => ({
-      lat: 39.193337569787815,
-      lng: -84.24132654605579,
-    }),
-    []
-  );
+	const center = useMemo(
+		() => ({
+			lat: 39.193337569787815,
+			lng: -84.24132654605579,
+		}),
+		[]
+	);
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  });
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+	});
 
-  if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
+	if (!isLoaded) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <GoogleMap
-      zoom={15}
-      center={center}
-      mapContainerClassName="w-full min-h-screen bg-stone-200"
-      options={{
-        styles: [
-          {
-            featureType: "poi",
-            elementType: "lables",
-            stylers: [
-              {
-                visibility: "off",
-              },
-            ],
-          },
-        ],
-      }}
-    >
-      <MarkerF position={center} />
-    </GoogleMap>
-  );
+	return (
+		<GoogleMap
+			zoom={15}
+			center={center}
+			mapContainerClassName="w-full min-h-screen bg-stone-200"
+			options={{
+				styles: [
+					{
+						featureType: 'poi',
+						elementType: 'lables',
+						stylers: [
+							{
+								visibility: 'off',
+							},
+						],
+					},
+				],
+			}}
+		>
+			<MarkerF position={center} />
+		</GoogleMap>
+	);
 }
